535efb779371a43c2bff784aed21fa140060ba65
wbadam <wagadam@gmail.com>
2019-07-10 18:57:32 +1200

    Add example data

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into customer (id, name) values ('c520b432a2dc11e9a2a32a2ae2dbcce4', 'Customer one');
+insert into customer (id, name) values ('9fc16c067b6b4ec89ab9a14ff342a7c0', 'Customer two');
+insert into customer (id, name) values ('c76282cf99f844049973deb81a46c662', 'Customer three');
+insert into customer (id, name) values ('d705dfb3fc8b4ac19466c925aac64dc0', 'Customer four');
