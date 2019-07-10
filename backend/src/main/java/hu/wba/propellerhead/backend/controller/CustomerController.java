e11fda34088fe2d80bb675bbb2a0a36ff2ab87ac
wbadam <wagadam@gmail.com>
2019-07-10 22:10:28 +1200

    Add API to filter customers and get customer by id

--- backend/src/main/java/hu/wba/propellerhead/backend/controller/CustomerController.java
+++ backend/src/main/java/hu/wba/propellerhead/backend/controller/CustomerController.java
@@ -2,8 +2,11 @@ package hu.wba.propellerhead.backend.controller;
 
 import hu.wba.propellerhead.backend.models.Customer;
 import hu.wba.propellerhead.backend.services.CustomerService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,7 +22,20 @@ public class CustomerController {
     }
 
     @RequestMapping("/customers")
-    public List<Customer> getCustomers() {
-        return customerService.listCustomers();
+    public List<Customer> getCustomers(@RequestParam("name") String name) {
+        List<Customer> customers;
+
+        if (StringUtils.isNotEmpty(name)) {
+            customers = customerService.listCustomersFilteredByName(name);
+        } else {
+            customers = customerService.listCustomers();
+        }
+
+        return customers;
+    }
+
+    @RequestMapping("/customers/{id}")
+    public Customer getCustomer(@PathVariable String id) {
+        return customerService.getCustomer(id).orElse(null);
     }
 }
--- backend/src/main/java/hu/wba/propellerhead/backend/services/CustomerService.java
+++ backend/src/main/java/hu/wba/propellerhead/backend/services/CustomerService.java
@@ -3,12 +3,22 @@ package hu.wba.propellerhead.backend.services;
 import hu.wba.propellerhead.backend.models.Customer;
 import hu.wba.propellerhead.backend.repositories.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class CustomerService {
+
+    // ExampleMatcher is immutable, hence reusable by threads
+    private static final ExampleMatcher STRING_MATCHER_CONTAINING_IGNORE_CASE = ExampleMatcher
+            .matchingAll()
+            .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING)
+            .withIgnoreCase();
 
     private final CustomerRepository repository;
 
@@ -19,5 +29,18 @@ public class CustomerService {
 
     public List<Customer> listCustomers() {
         return repository.findAll();
+    }
+
+    public List<Customer> listCustomersFilteredByName(String name) {
+        Customer probe = new Customer();
+        probe.setName(name);
+
+        Example<Customer> example = Example.of(probe, STRING_MATCHER_CONTAINING_IGNORE_CASE);
+
+        return repository.findAll(example);
+    }
+
+    public Optional<Customer> getCustomer(String id) {
+        return repository.findById(UUID.fromString(id));
     }
 }
