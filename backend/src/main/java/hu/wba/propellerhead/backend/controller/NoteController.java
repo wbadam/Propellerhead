52258c97328c4d3a85544cf06bf4c4295a547f5c
wbadam <wagadam@gmail.com>
2019-07-14 12:58:34 +1200

    Allow frontend origin

--- backend/src/main/java/hu/wba/propellerhead/backend/controller/NoteController.java
+++ backend/src/main/java/hu/wba/propellerhead/backend/controller/NoteController.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class NoteController {
 
     private final NoteService noteService;
